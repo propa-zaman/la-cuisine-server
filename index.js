6f9634eb914485a325d10bd37c57972a245b3df3
Afra <propazaman12@gmail.com>
2023-05-04 10:48:31 +0600

    data loaded in the index.js

--- index.js
+++ index.js
@@ -5,12 +5,20 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 6001;
 
+// loading data
+const chefDetails = require('./data/chefDetails.json')
+
 // to call from other api
 app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('La cuisine is running');
 });
+
+// all categories data
+app.get('/chefDetails', (req, res) => {
+    res.send(chefDetails);
+  })
 
 app.listen(port, () => {
   console.log(`cuisine api is running on the port ${port}`);
